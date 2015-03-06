e2c09104cce7b7c573c6ed18fc35a3a161e91961
philipmward@hotmail.com <philipmward@hotmail.com>
2015-03-06 15:38:59 -0700

    Commit

--- ControllerExtensibility/ControllerExtensibility/Global.asax.cs
+++ ControllerExtensibility/ControllerExtensibility/Global.asax.cs
@@ -1,16 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using ControllerExtensibility.Infrastructure;
+using System;
 using System.Web;
-using System.Web.Security;
-using System.Web.SessionState;
+using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace ControllerExtensibility
 {
-    public class Global : System.Web.HttpApplication
+    public class Global : HttpApplication
     {
         protected void Application_Start(object sender, EventArgs e)
         {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            //ControllerBuilder.Current.SetControllerFactory(new CustomControllerFactory());
+
+            //ControllerBuilder.Current.DefaultNamespaces.Add("MyControllerNamespace");
+            //ControllerBuilder.Current.DefaultNamespaces.Add("MyProject.*");
+
+            ControllerBuilder.Current.SetControllerFactory(new DefaultControllerFactory(new CustomControllerActivator()));
         }
     }
 }
---
+++ ControllerExtensibility/ControllerExtensibility/Infrastructure/CustomControllerActivator.cs
@@ -0,0 +1,24 @@
+﻿using ControllerExtensibility.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ControllerExtensibility.Infrastructure
+{
+    public class CustomControllerActivator : IControllerActivator
+    {
+        public IController Create(RequestContext requestContext, Type controllerType)
+        {
+            //Would never do anything like this in a real project. Just used to show how you can intercept requests between the ControllerFactory and the dependancy resolver.
+            if (controllerType == typeof(ProductController))
+            {
+                controllerType = typeof(CustomerController);
+            }
+            return (IController)DependencyResolver.Current.GetService(controllerType);
+        }
+    }
+}
